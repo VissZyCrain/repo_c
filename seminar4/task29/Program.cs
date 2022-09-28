008c50e9f38d49ed98d2a1a8320cf8fb3175ff05
VissZyCrain <aletuchin@yandex.ru>
2022-09-28 18:18:38 +0300

    realized task 29

---
+++ seminar4/task29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. 
+// Данные вводятся с консоли пользователем
+
+Console.Clear();
+Console.WriteLine("Задача 29 ");
+int [] array = new int [8];
+int ZaprosChisel (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int num = 1;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = ZaprosChisel("Введите число " + num);
+    num = num + 1;
+}
+Console.WriteLine(string.Join(", ", array));
