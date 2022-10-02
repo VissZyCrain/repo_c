dcf6a4cc635023ca58c2f7bde7cf2001993d980e
VissZyCrain <aletuchin@yandex.ru>
2022-10-03 00:37:42 +0300

    realized task 41

---
+++ seminar6/task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.WriteLine("Задача 41 ");
+Console.WriteLine ("Введите числа:");
+Console.WriteLine ("Ввод чисел останавливается при помощи STOP");
+
+int countOfPositive = 0;
+
+while (true)
+{
+  string answer = Console.ReadLine();
+  if (answer == "STOP")
+  {
+    break;
+  }
+  else
+  {
+    int answerNumber = Convert.ToInt32(answer);
+    if (answerNumber > 0)
+    {
+      countOfPositive++;
+    }
+
+  }
+}
+Console.WriteLine($"Количество чисел больше 0: "); 
+Console.WriteLine(countOfPositive); 
