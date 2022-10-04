93044c74b1693e41937b4c285720914a039c1869
VissZyCrain <aletuchin@yandex.ru>
2022-10-04 17:52:01 +0300

    realized task 64

---
+++ finaltasks/task64/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+Console.WriteLine("Задача 64");
+int n = InputInt("Введите положительное число");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
